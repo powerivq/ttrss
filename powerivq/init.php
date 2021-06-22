9a5252e63cdcbc50d3eefe54d6f247b802808ce1
powerivq <poweriv.q@gmail.com>
2021-06-22 13:49:56 -0500

    Add google reader key binding

--- powerivq/init.php
+++ powerivq/init.php
@@ -11,10 +11,22 @@ class PowerIVQ extends Plugin {
 
     function init($host) {
         $this->host = $host;
+        $host->add_hook($host::HOOK_HOTKEY_MAP, $this);
     }
 
     function get_js() {
         return file_get_contents(dirname(__FILE__) . '/init.js');
+    }
+
+    function hook_hotkey_map($hotkeys) {
+        $hotkey_overrides = array(
+            "j" => "next_article_noscroll",
+            "J" => "next_article_noscroll",
+            "k" => "prev_article_noscroll",
+            "K" => "prev_article_noscroll"
+        );
+
+        return array_merge($hotkeys, $hotkey_overrides);
     }
 
     function api_version() {
